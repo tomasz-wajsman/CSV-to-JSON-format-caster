1a100d1fce60637e69bdccab6b79d5b0c7768ebf
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-06-08 16:55:10 +0200

    Added caster scripts

--- caster/to-csv.js
+++ caster/to-csv.js
@@ -1,4 +1,42 @@
-const toCSV = contents => {
+const digitSeparator = '.';
+const defaultSeparator = ',';
 
+const parse = value => {
+  // check if not undefined
+  if (value === undefined) {
+    return "null";
+  }
+  return String(value);
 };
-module.exports = toCSV;
+const convert = (contents, separator = defaultSeparator) => {
+  // get header
+  const keys = [];
+  const csv = [];
+  if (contents) {
+    contents.forEach(entry => {
+      // add entries
+      let line = '';
+      Object.keys(entry).forEach(key => {
+        keys.push(key);
+        line += `${parse(entry[key])}${separator}`;
+      });
+      // remove the last character and push
+      csv.push(line.substring(0, line.length - 1));
+    });
+    // make keys unique
+    const keysUniq = new Set(keys);
+    // add keys in front
+    let keysLine = '';
+    Array.from(keysUniq).forEach(key => {
+      keysLine += `${key}${separator}`;
+    });
+    csv.unshift(keysLine.substring(0, keysLine.length - 1));
+    return csv;
+  } else {
+    return false;
+  }
+};
+
+module.exports = {
+  parse, convert
+};
--- caster/to-json.js
+++ caster/to-json.js
@@ -1,4 +1,5 @@
 const digitSeparator = '.';
+const defaultSeparator = ',';
 
 const parse = value => {
   // null, empty string, undefined, false and true
@@ -32,8 +33,48 @@ const parse = value => {
     return number;
   }
 };
-const convert = contents => {
 
+const convert = (contents, separator = defaultSeparator) => {
+  if (Array.isArray(contents)) {
+    // get the definitions
+    const json = [];
+    const defTemp = String(contents[0]);
+    const defSplit = defTemp.split(separator);
+    // build keys
+    const keys = [];
+    defSplit.forEach(definition => keys.push(definition));
+    // loop over every line of the contents
+    for (let i = 1; i < contents.length; i++) {
+      const obj = {};
+      const values = String(contents[i]);
+      const valTemp = values.split(separator);
+      valTemp.forEach((value, position) => {
+        const parsed = parse(value);
+        // check if value is not undefined
+        if (parsed !== undefined) {
+          // the value is defined
+          // check if the key can be split into parts
+          const key = String(keys[position]);
+          const keySplit = key.split('-');
+          const levels = keySplit.length;
+          obj[keys[position]] = parsed;
+          /*
+          if (levels > 1) {
+            // add hierarchy
+            let currentLevel = 1;
+            obj[keys[position]] = parsed;
+          } else {
+            obj[keys[position]] = parsed;
+          }
+          */
+        }
+      });
+      json.push(obj);
+    }
+    return json;
+  } else {
+    return false;
+  }
 };
 module.exports = {
   parse, convert
