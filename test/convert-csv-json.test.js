eee70d99c436bc9fcbfa535b6be56bebcadf1fd0
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-06-08 14:38:01 +0200

    Added more test cases, test fix
    Fixed the test because comparison didn't work on objects

--- test/convert-csv-json.test.js
+++ test/convert-csv-json.test.js
@@ -7,16 +7,29 @@ const mocks = [
     'John,Doe',
     'Jan,Kowalski',
     'Abc,Xyz'
+  ],
+  [
+    'data1,data2',
+    'false,true',
+    'undefined,null'
+  ],
+  [
+    'string,number,bool',
+    'Foo,123,false',
+    'Bar,-1,true',
+    'Baz,1.23,false'
   ]
 ];
 const JSONs = [
-  JSON.parse('[{"first_name": "John","last_name": "Doe"},{"first_name": "Jan","last_name": "Kowalski"},{"first_name": "Abc","last_name": "Xyz"}]')
+  JSON.parse('[{"first_name": "John","last_name": "Doe"},{"first_name": "Jan","last_name": "Kowalski"},{"first_name": "Abc","last_name": "Xyz"}]'),
+  JSON.parse('[{"data1": false,"data2": true},{"data2": null}]'),
+  JSON.parse('[{"string": "Foo", "number": 123, "bool": false}, {"string": "Bar", "number": -1, "bool": true}, {"string": "Baz", "number": 1.23, "bool": false}]')
 ];
 
 describe('CSV to JSON converting', () => {
   test('Converts strings to JSON object', () => {
     mocks.forEach((mock, id) => {
-      assert.equal(Caster.toJSON.convert(mock, ','), JSONs[id], `The ${id + 1} mock was not converted properly`)
+      assert.equal(JSON.stringify(Caster.toJSON.convert(mock, ',')), JSON.stringify(JSONs[id]), `The #${id + 1} mock was not converted properly`)
     });
   });
 });
